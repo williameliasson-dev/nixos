f9bfb5655cf98d8bcb17dbc09ef55ed6c545b099
williameliasson-dev <williameliasson5@gmail.com>
2025-10-02 23:16:45 +0200

    test2

--- modules/shared/kitty.nix
+++ modules/shared/kitty.nix
@@ -1,92 +1,89 @@
-{
-  programs.kitty = {
-    enable = true;
-    settings = {
-      # General settings
-      font_family = "FiraCode Nerd Font Mono";
-      font_size = 12;
-      adjust_line_height = 0;
-      adjust_column_width = 0;
-      disable_ligatures = "never";
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  home.file.".config/kitty/kitty.conf".text = ''
+    # General settings
+    font_family FiraCode Nerd Font Mono
+    font_size 12
+    adjust_line_height 0
+    adjust_column_width 0
+    disable_ligatures never
 
-      # Window layout
-      window_padding_width = 4;
-      hide_window_decorations = "no";
+    # Window layout
+    window_padding_width 4
+    hide_window_decorations no
 
-      # Color scheme
-      background = "#262626";
-      foreground = "#D4D4D4";
-      cursor = "#FFFFFF";
+    # Color scheme
+    background #262626
+    foreground #D4D4D4
+    cursor #FFFFFF
 
-      # Other settings
-      enable_audio_bell = false;
-      update_check_interval = 0;
-      allow_remote_control = true;
-      
-      # Terminal compatibility for SSH
-      term = "xterm-256color";
-    };
+    # Other settings
+    enable_audio_bell no
+    update_check_interval 0
+    allow_remote_control yes
 
-    keybindings = {
-      "ctrl+shift+c" = "copy_to_clipboard";
-      "ctrl+shift+v" = "paste_from_clipboard";
-    };
+    # Terminal compatibility for SSH
+    term xterm-256color
 
-    extraConfig = ''
-      # Any additional configuration that doesn't fit into the settings attribute
+    # Wayland
+    linux_display_server wayland
 
-      background_opacity 0.9
+    # Keybindings
+    map ctrl+shift+c copy_to_clipboard
+    map ctrl+shift+v paste_from_clipboard
 
-      # For example, complex key mappings:
-      map ctrl+shift+d new_tab_with_cwd
+    # Additional configuration
+    background_opacity 0.9
+    map ctrl+shift+d new_tab_with_cwd
 
-      # Gruvbox Dark color scheme
+    # Gruvbox Dark color scheme
+    # The basic colors
+    foreground              #ebdbb2
+    background              #282828
+    selection_foreground    #655b53
+    selection_background    #ebdbb2
 
-      # The basic colors
-      foreground              #ebdbb2
-      background              #282828
-      selection_foreground    #655b53
-      selection_background    #ebdbb2
+    # Cursor colors
+    cursor                  #8ec07c
+    cursor_text_color       #1d2021
 
-      # Cursor colors
-      cursor                  #8ec07c
-      cursor_text_color       #1d2021
+    # URL underline color when hovering with mouse
+    url_color               #d65d0e
 
-      # URL underline color when hovering with mouse
-      url_color               #d65d0e
+    # The basic 16 colors
+    # black
+    color0                  #282828
+    color8                  #928374
 
-      # The basic 16 colors
-      # black
-      color0                  #282828
-      color8                  #928374
+    # red
+    color1                  #cc241d
+    color9                  #fb4934
 
-      # red
-      color1                  #cc241d
-      color9                  #fb4934
+    # green
+    color2                  #98971a
+    color10                 #b8bb26
 
-      # green
-      color2                  #98971a
-      color10                 #b8bb26
+    # yellow
+    color3                  #d79921
+    color11                 #fabd2f
 
-      # yellow
-      color3                  #d79921
-      color11                 #fabd2f
+    # blue
+    color4                  #458588
+    color12                 #83a598
 
-      # blue
-      color4                  #458588
-      color12                 #83a598
+    # magenta
+    color5                  #b16286
+    color13                 #d3869b
 
-      # magenta
-      color5                  #b16286
-      color13                 #d3869b
+    # cyan
+    color6                  #689d6a
+    color14                 #8ec07c
 
-      # cyan
-      color6                  #689d6a
-      color14                 #8ec07c
-
-      # white
-      color7                  #a89984
-      color15                 #ebdbb2
-    '';
-  };
+    # white
+    color7                  #a89984
+    color15                 #ebdbb2
+  '';
 }
