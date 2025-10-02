191a2e62e5c2d55448089598dbfc212a36d803be
williameliasson-dev <williameliasson5@gmail.com>
2025-10-02 23:22:05 +0200

    test3

--- modules/shared/zsh.nix
+++ modules/shared/zsh.nix
@@ -1,49 +1,44 @@
-{ pkgs, ... }:
-let
-  # Define any local variables here
-  customAliases = {
-    vim = "nvim";
-    ll = "ls -l";
-    update = "sudo nixos-rebuild switch --flake /home/william/nixos#";
-    nixflake = "cd /home/william/nixos && nvim flake.nix";
-    nas = "sudo sshfs -o IdentityFile=~/.ssh/homelab/id_ed25519 sftp@homelab.local:/mnt/storage ~/nas";
-    nas-umount = "umount ~/nas";
-    rel-notes = "git --no-pager show --pretty=format:%s -s tags/$(git describe --tags --abbrev=0)..HEAD";
-  };
-in
-{
-  # Enable Zsh system-wide
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "af-magic";
-    };
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  home.file.".zshrc".text = ''
+    # Install Oh-My-Zsh manually with:
+    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    shellAliases = customAliases;
+    export ZSH="$HOME/.oh-my-zsh"
+    ZSH_THEME="af-magic"
+    plugins=(git)
 
-    # Additional Zsh configuration
-    initContent = ''
-      fastfetch
-      # Set PATH
-      export PATH=$HOME/.local/bin:$PATH
+    if [ -f "$ZSH/oh-my-zsh.sh" ]; then
+      source $ZSH/oh-my-zsh.sh
+    fi
 
-      # Custom key bindings
-      bindkey '^[[A' history-substring-search-up
-      bindkey '^[[B' history-substring-search-down
+    # Aliases
+    alias vim="nvim"
+    alias ll="ls -l"
+    alias update="home-manager switch --flake ~/.config/home-manager#william@desktop"
+    alias nixflake="cd ~/.config/home-manager && nvim flake.nix"
+    alias nas="sudo sshfs -o IdentityFile=~/.ssh/homelab/id_ed25519 sftp@homelab.local:/mnt/storage ~/nas"
+    alias nas-umount="umount ~/nas"
+    alias rel-notes="git --no-pager show --pretty=format:%s -s tags/\$(git describe --tags --abbrev=0)..HEAD"
 
-      # Set default editor
-      export EDITOR='nvim'
+    fastfetch
 
-      # Custom prompt (if not using Oh-My-Zsh theme)
-      # PROMPT='%F{green}%n@%m%f:%F{blue}%~%f$ '
+    export PATH=$HOME/.local/bin:$PATH
+    export EDITOR='nvim'
 
-      # Add any other custom Zsh configurations here
-    '';
-  };
+    # Load Nix zsh plugins if available
+    [ -f ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    [ -f ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-  # Ensure Zsh is available
-  home.packages = with pkgs; [ zsh ];
+    bindkey '^[[A' history-substring-search-up
+    bindkey '^[[B' history-substring-search-down
+  '';
+
+  home.packages = with pkgs; [
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+  ];
 }
