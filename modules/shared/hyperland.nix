165a0bed6176c41feb5ffa26c94776eea8a25a2d
williameliasson-dev <williameliasson5@gmail.com>
2025-10-02 23:07:34 +0200

    test

--- modules/shared/hyperland.nix
+++ modules/shared/hyperland.nix
@@ -1,204 +1,190 @@
-{
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    settings = {
-      monitor = [
-        "DP-3,3440x1440@99.9,0x0,1"
-        "DP-5,3440x1440@59.97,0x0,1"
-        "HDMI-A-1,3440x1440@99.9,0x0,1"
-        ",preferred,auto,1"
-        "eDP-1,1920x1200@60,760x1440,1"
-      ];
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  home.file.".config/hypr/hyprland.conf".text = ''
+    # Monitors
+    monitor=DP-3,3440x1440@99.9,0x0,1
+    monitor=DP-5,3440x1440@59.97,0x0,1
+    monitor=HDMI-A-1,3440x1440@99.9,0x0,1
+    monitor=,preferred,auto,1
+    monitor=eDP-1,1920x1200@60,760x1440,1
 
-      # Set programs that you use
-      "$terminal" = "kitty";
-      "$fileManager" = "yazi";
-      "$menu" = "rofi -show drun";
-      "$windows" = "rofi -show window";
-      "$mainMod" = "SUPER";
+    # Programs
+    $terminal = kitty
+    $fileManager = yazi
+    $menu = rofi -show drun
+    $windows = rofi -show window
+    $mainMod = SUPER
 
-      # Some default env vars
-      env = [
-        "XCURSOR_SIZE,24"
-        "XCURSOR_THEME,Adwaita"
-        "NIXOS_OZONE_WL=1"
-      ];
+    # Environment variables
+    env = XCURSOR_SIZE,24
+    env = XCURSOR_THEME,Adwaita
+    env = NIXOS_OZONE_WL,1
 
-      # Input configuration
-      input = {
-        kb_layout = "se";
-        kb_variant = "";
-        kb_model = "";
-        kb_options = "";
-        kb_rules = "";
-        follow_mouse = 1;
-        touchpad = {
-          natural_scroll = "no";
-        };
-        sensitivity = 0;
-      };
+    # Input configuration
+    input {
+        kb_layout = se
+        follow_mouse = 1
+        touchpad {
+            natural_scroll = no
+        }
+        sensitivity = 0
+    }
 
-      # General configuration
-      general = {
-        gaps_in = 0;
-        gaps_out = 0;
-        border_size = 1;
-        "col.active_border" = "rgba(689D6ACC)";
-        layout = "dwindle";
-        allow_tearing = false;
-      };
+    # General configuration
+    general {
+        gaps_in = 0
+        gaps_out = 0
+        border_size = 1
+        col.active_border = rgba(689D6ACC)
+        layout = dwindle
+        allow_tearing = false
+    }
 
-      # Decoration configuration
-      decoration = {
-        rounding = 0;
+    # Decoration
+    decoration {
+        rounding = 0
+        blur {
+            enabled = true
+            size = 3
+            passes = 1
+        }
+        shadow {
+            enabled = true
+            range = 4
+            render_power = 3
+            color = rgba(1a1a1aee)
+        }
+    }
 
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-        };
+    # Animations
+    animations {
+        enabled = true
+        bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+        animation = windows, 1, 7, myBezier
+        animation = windowsOut, 1, 7, default, popin 80%
+        animation = border, 1, 10, default
+        animation = borderangle, 1, 8, default
+        animation = fade, 1, 7, default
+        animation = workspaces, 1, 6, default
+    }
 
-        shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(1a1a1aee)";
-        };
-      };
+    # Dwindle layout
+    dwindle {
+        pseudotile = true
+        preserve_split = true
+    }
 
-      # Animations configuration
-      animations = {
-        enabled = true;
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
-      };
+    # Master layout
+    master {
+        new_status = master
+    }
 
-      # Dwindle layout configuration
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
+    # Misc
+    misc {
+        force_default_wallpaper = -1
+    }
 
-      # Master layout configuration
-      master = {
-        new_status = "master";
-      };
+    # Application launching
+    bind = $mainMod, Q, exec, $terminal
+    bind = $mainMod, C, killactive,
+    bind = $mainMod, M, exit,
+    bind = $mainMod, E, exec, $fileManager
+    bind = $mainMod, V, togglefloating,
+    bind = $mainMod, D, exec, $menu
+    bind = $mainMod, TAB, exec, $windows
+    bind = $mainMod, P, pseudo,
+    bind = $mainMod, J, togglesplit,
+    bind = $mainMod, F, fullscreen
 
-      # Misc configuration
-      misc = {
-        force_default_wallpaper = -1;
-      };
+    # Move focus with vim binds
+    bind = $mainMod, H, movefocus, l
+    bind = $mainMod, L, movefocus, r
+    bind = $mainMod, K, movefocus, u
+    bind = $mainMod, J, movefocus, d
 
-      # Key bindings
-      bind = [
-        # Application launching
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, D, exec, $menu"
-        "$mainMod, TAB, exec, $windows"
-        "$mainMod, P, pseudo,"
-        "$mainMod, J, togglesplit,"
-        "$mainMod, F, fullscreen"
+    # Move window with vim binds
+    bind = $mainMod SHIFT, H, movewindow, l
+    bind = $mainMod SHIFT, L, movewindow, r
+    bind = $mainMod SHIFT, K, movewindow, u
+    bind = $mainMod SHIFT, J, movewindow, d
 
-        # Move focus with mainMod + vim binds
-        "$mainMod, H, movefocus, l"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, J, movefocus, d"
+    # Switch workspaces
+    bind = $mainMod, 1, workspace, 1
+    bind = $mainMod, 2, workspace, 2
+    bind = $mainMod, 3, workspace, 3
+    bind = $mainMod, 4, workspace, 4
+    bind = $mainMod, 5, workspace, 5
+    bind = $mainMod, 6, workspace, 6
+    bind = $mainMod, 7, workspace, 7
+    bind = $mainMod, 8, workspace, 8
+    bind = $mainMod, 9, workspace, 9
+    bind = $mainMod, 0, workspace, 10
 
-        # Move window with mainMod + vim binds
-        "$mainMod SHIFT, H, movewindow, l"
-        "$mainMod SHIFT, L, movewindow, r"
-        "$mainMod SHIFT, K, movewindow, u"
-        "$mainMod SHIFT, J, movewindow, d"
+    # Screenshot
+    bind = $mainMod SHIFT, P, exec, bash -c 'grim -g "$(slurp -d)" - | wl-copy'
 
-        # Switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
+    # Move window to workspace
+    bind = $mainMod SHIFT, 1, movetoworkspace, 1
+    bind = $mainMod SHIFT, 2, movetoworkspace, 2
+    bind = $mainMod SHIFT, 3, movetoworkspace, 3
+    bind = $mainMod SHIFT, 4, movetoworkspace, 4
+    bind = $mainMod SHIFT, 5, movetoworkspace, 5
+    bind = $mainMod SHIFT, 6, movetoworkspace, 6
+    bind = $mainMod SHIFT, 7, movetoworkspace, 7
+    bind = $mainMod SHIFT, 8, movetoworkspace, 8
+    bind = $mainMod SHIFT, 9, movetoworkspace, 9
+    bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
-        # Screenshot draggable
-        "$mainMod SHIFT, P, exec, bash -c 'grim -g \"$(slurp -d)\" - | wl-copy'"
+    # Scratchpad
+    bind = $mainMod, S, togglespecialworkspace, magic
+    bind = $mainMod SHIFT, S, movetoworkspace, special:magic
 
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
+    # Workspace navigation with scroll
+    bind = $mainMod, mouse_down, workspace, e+1
+    bind = $mainMod, mouse_up, workspace, e-1
 
-        # Scratchpad
-        "$mainMod, S, togglespecialworkspace, magic"
-        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+    # Volume
+    bind = , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+
+    bind = , XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    bind = , XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
 
-        # Workspace navigation with scroll
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
+    # Brightness
+    bind = , XF86MonBrightnessDown, exec, xbacklight -dec 5
+    bind = , XF86MonBrightnessUp, exec, xbacklight -inc 5
 
-        # Volume binds
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        #Brightness
-        ", XF86MonBrightnessDown, exec, xbacklight -dec 5"
-        ", XF86MonBrightnessUp, exec, xbacklight -inc 5"
+    # Lock screen
+    bind = $mainMod, O, exec, hyprlock
 
-        # Lock Screen
-        "$mainMod, O, exec, hyprlock"
+    # Power menu
+    bind = $mainMod, ESCAPE, exec, rofi -show power-menu -modi power-menu:rofi-power-menu
 
-        # Power menu
-        "$mainMod, ESCAPE, exec, rofi -show power-menu -modi power-menu:rofi-power-menu"
+    # Fine resize controls
+    bind = $mainMod ALT, H, resizeactive, -50 0
+    bind = $mainMod ALT, L, resizeactive, 50 0
+    bind = $mainMod ALT, K, resizeactive, 0 -50
+    bind = $mainMod ALT, J, resizeactive, 0 50
 
-        # Window sizing shortcuts
-        # Fine resize controls
-        "$mainMod ALT, H, resizeactive, -50 0"
-        "$mainMod ALT, L, resizeactive, 50 0"
-        "$mainMod ALT, K, resizeactive, 0 -50"
-        "$mainMod ALT, J, resizeactive, 0 50"
+    # Larger resize steps
+    bind = $mainMod CTRL ALT, H, resizeactive, -200 0
+    bind = $mainMod CTRL ALT, L, resizeactive, 200 0
+    bind = $mainMod CTRL ALT, K, resizeactive, 0 -200
+    bind = $mainMod CTRL ALT, J, resizeactive, 0 200
 
-        # Larger resize steps
-        "$mainMod CTRL ALT, H, resizeactive, -200 0"
-        "$mainMod CTRL ALT, L, resizeactive, 200 0"
-        "$mainMod CTRL ALT, K, resizeactive, 0 -200"
-        "$mainMod CTRL ALT, J, resizeactive, 0 200"
-      ];
+    # Mouse bindings
+    bindm = $mainMod, mouse:272, movewindow
+    bindm = $mainMod, mouse:273, resizewindow
 
-      # Mouse bindings
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
+    # Startup applications
+    exec-once = waybar
+    exec-once = hyprctl setcursor Adwaita 24
+    exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+  '';
 
-      # Startup applications
-      "exec-once" = [
-        "waybar"
-        "hyprctl setcursor Adwaita 24"
-        "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      ];
-    };
+  home.sessionVariables = {
+    KITTY_ENABLE_WAYLAND = "1";
   };
 }
